0493025370cc045bc37036d27a532713b6e7519d
Hannah161jung <junghan1@students.zhaw.ch>
2026-04-21 15:05:29 +0200

    neue ordner und files

---
+++ views/home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Lernapp")
+
+"""
+Diese App wurde von folgenden Personen entwickelt:
+
+- Darlene Armenio : [armdar01@students.zhaw.ch]
+- Kipisha Selvan : [selvakip@students.zhaw.ch]
+- Hannah Jung : [junghan1@students.zhaw.ch]
+- Mimoza Mehmeti: [mehmemim@students.zhaw.ch]
+
+
+
+Dozent:
+- Wehrli Samuel: [wehs@zhaw.ch]
+- Fox Paul: [foxp@zhaw.ch]
+- Ho Ka Men : [hokam001@students.zhaw.ch]
+
+"""
