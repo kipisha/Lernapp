3c2940f7ce4b532c9eafdad4b70ca18456fc2e55
Hannah161jung <junghan1@students.zhaw.ch>
2026-04-21 15:19:07 +0200

    app.py code gemacht mal

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_second = st.Page("views/weeklyview.py", title="Wochenansicht", icon=":material/info:")
+pg_weeklyview = st.Page("views/weeklyview.py", title="Wochenansicht", icon=":material/info:")
+pg_progresspoints = st.Page("views/progresspoints.py", title="Punktefortschritt", icon=":material/info:")
 
-pg = st.navigation([pg_home, pg_second])
+pg = st.navigation([pg_home, pg_weeklyview, pg_progresspoints])
 pg.run()
