d3d631768fcbf56eba7b0157f8cf1008d4415c94
Hannah161jung <junghan1@students.zhaw.ch>
2026-04-21 15:41:59 +0200

    restliche dokumente erstellt und code reinkopiert für app gerüst

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+st.set_page_config(page_title="Lernapp", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_weeklyview = st.Page("views/weeklyview.py", title="Wochenansicht", icon=":material/info:")
